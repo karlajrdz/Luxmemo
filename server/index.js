bc9e9e731aa98887cb2e2a22d7e1ad861fffcdac
Tu Nombre <karlajrdz@gmail.com>
2022-07-06 01:51:17 +0200

    second commit

--- server/index.js
+++ server/index.js
@@ -3,24 +3,23 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 //importing the routes posts
-import postRoutes from "./routes/posts.js";
+import postRoutes from './routes/posts.js';
 
 //inicialize the app with a function
 const app = express();
 //settin up  the starting path for the routes in postjs
 
-
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use('/posts', postRoutes);
 
 
 
 const CONNECTION_URL =
   "mongodb+srv://Karla:karla123@cluster0.qug8bcu.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 console.log('server running on port : ${PORT}')
 
 //connect database first a parameter and then a object with options
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,4 +6,6 @@ const router = express.Router();
 router.get("/", getPosts);
 router.post("/", createPost);
 
+
+
 export default router;
